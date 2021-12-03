e30dcfea27391531e5461d15ea3d2a5b4be31eff
tuannguyen2k5 <nguyentientuan20052000@gmail.com>
2021-12-03 11:02:14 +0700

    angular_example

---
+++ src/app/models/alert.model.ts
@@ -0,0 +1,14 @@
+export interface Alert {
+    id: string;
+    type: AlertType;
+    message: string;
+    autoClose: boolean;
+    keepAfterRouteChange: boolean;
+    fade: boolean;
+}
+export enum AlertType {
+    Success,
+    Error,
+    Info,
+    Warning
+}
